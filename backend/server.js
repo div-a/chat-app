839ec7099c4349b716c303ef4855dd550f3784c9
Diva Lonial <diva.lonial@gmail.com>
2021-06-02 20:14:48 +0800

    fix cors error and imports

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,26 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
-var user = require('./utils/user');
+var cors = require('cors')
+var userHelper = require('./utils/user');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST"]
+    }
+});
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors);
 
 io.on('connection', socket => {
+    console.log('connection')
     socket.on('joinRoom', ({ username, room }) => {
-        const user = userJoin(socket.id, username, room);
+        const user = userHelper.userJoin(socket.id, username, room);
         socket.join(user.room);
         socket.emit("message", "Welcome to the room");
         socket.broadcast
@@ -22,12 +30,12 @@ io.on('connection', socket => {
     });
 
     socket.on('chatMessage', (msg) => {
-        const user = getCurrentUser(socket.id);
+        const user = userHelper.getCurrentUser(socket.id);
         io.to(user.room).emit("message", { user: "user", msg });
     });
 
     socket.on('disconnect', () => {
-        const user = userLeaves(socket.id);
+        const user = userHelper.userLeaves(socket.id);
 
         if (user) {
             io.to(user.room).emit("message", `${username} has left the room`);
--- backend/utils/user.js
+++ backend/utils/user.js
@@ -2,7 +2,7 @@ const users = [];
 
 function userJoin(id, username, room) {
     const user = { id, username, room };
-    users.push(hser);
+    users.push(user);
     return user;
 }
 
